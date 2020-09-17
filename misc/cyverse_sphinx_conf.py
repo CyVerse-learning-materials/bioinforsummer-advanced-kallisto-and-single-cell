01c6407111e092d4e76d5e9b90adc76d9138b125
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2020-09-17 14:44:06 -0400

    add triage issue template

--- misc/cyverse_sphinx_conf.py
+++ misc/cyverse_sphinx_conf.py
@@ -10,10 +10,10 @@ extensions = [
 ]
 
 project = 'BioInfoSummer18 - Advanced Kallisto RNA-Seq (Bulk and Single Cell)'
-copyright = '2018, CyVerse'
+copyright = '2020, CyVerse'
 author = 'CyVerse'
-version = '1.0'
-release = '1.0'
+version = '2.0'
+version = '2.0'
 
 language = None
 source_parsers = {
@@ -74,3 +74,7 @@ def setup(app):
     #uncomment to enable table sorting app.add_javascript('jquery.tablesorter.min.js')
     app.add_javascript('cyverse.js')
     app.add_javascript('intercom-script-for-learning.js')
+    app.add_stylesheet('detail-expand.css')
+    app.add_stylesheet('question-answer.css')
+    app.add_javascript('detail-expand.js')
+    app.add_javascript('question-answer.js')
